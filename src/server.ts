537aefc9130be55b9811cadee55c20fb1e7d51e7
rick <philipefdev@gmail.com>
2025-12-27 14:39:37 -0300

    feat: finalize auth and database connection with atlas

---
+++ src/server.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import routes from './routes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(routes);
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost:27017/linkio_challenge'
+
+// Conexão com o banco de dados
+mongoose.connect(MONGO_URI)
+    .then(() => {
+        console.log('Conectando ao MongoDB com sucesso!');
+        app.listen(PORT, () => {
+            console.log(`Servidor rodando em http:localhost:${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Erro ao conectar o MongoDB', err)
+    })
